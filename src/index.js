89dacd99d4338394b7ec6cf55e5ede3a663ee01d
mattpe <mattpe@metropolia.fi>
2024-10-28 10:35:30 +0200

    wip: migrate to express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import http from 'http';
+import express from 'express';
 import {getItems, postItem} from './items.js';
 const hostname = '127.0.0.1';
 const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+// /api/items resource endpoints
+app.get('/api/items', (req, res) => {
+  getItems(res);
+});
+app.post('/api/items', (req, res) => {
+  postItem(req, res);
+});
+app.put('/api/items', (req, res) => {
+  res.status(501).json({message: 'Under construction'});
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 // Create a server object and bind a callback function
 // to all request events
-const server = http.createServer((req, res) => {
+/* const server = http.createServer((req, res) => {
   const {url, method} = req;
   console.log('url:', url, 'method:', method);
   if (url === '/items' && method === 'GET') {
@@ -17,8 +35,6 @@ const server = http.createServer((req, res) => {
     res.writeHead(404, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({error: '404', message: 'not found'}));
   }
-});
+}); */
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
--- src/items.js
+++ src/items.js
@@ -5,27 +5,31 @@ const items = [
 ];
 
 const getItems = (res) => {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(items));
+  res.json(items);
 };
 
 const postItem = (req, res) => {
-  let body = [];
-  req
-    .on('data', (chunk) => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
-      // at this point, `body` has the entire request body stored in it as a string
-      console.log('req body:', body);
-      const item = JSON.parse(body);
-      // TODO: check largest id in array and increment by 1
-      item.id = items.length + 1; // NOT like this
-      items.push(item);
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({message: 'Item added'}));
-    });
+  console.log('post req body', req.body);
+  
+  res.status(201).json({message: 'Item added', id: 0});
+  // Status code only is a valid response too
+  // res.sendStatus(201);
+  
+  // req
+  //   .on('data', (chunk) => {
+  //     body.push(chunk);
+  //   })
+  //   .on('end', () => {
+  //     body = Buffer.concat(body).toString();
+  //     // at this point, `body` has the entire request body stored in it as a string
+  //     console.log('req body:', body);
+  //     const item = JSON.parse(body);
+  //     // TODO: check largest id in array and increment by 1
+  //     item.id = items.length + 1; // NOT like this
+  //     items.push(item);
+  //     res.writeHead(200, {'Content-Type': 'application/json'});
+  //     res.end(JSON.stringify({message: 'Item added'}));
+
 };
 
 export {getItems, postItem};
