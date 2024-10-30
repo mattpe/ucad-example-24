35ed9250874144967f672d235c2ae7b780e489de
mattpe <mattpe@metropolia.fi>
2024-10-30 13:32:07 +0200

    add: replace items -> media

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import express from 'express';
-import {getItemById, getItems, postItem} from './items.js';
+import {getItemById, getItems, postItem} from './media.js';
 const hostname = '127.0.0.1';
 const port = 3000;
 const app = express();
 
 app.use(express.json());
 
-// /api/items resource endpoints
-app.get('/api/items', (req, res) => {
+// Media resource endpoints
+app.get('/api/media', (req, res) => {
   getItems(res);
 });
-app.get('/api/items/:id', (req, res) => {
+app.get('/api/media/:id', (req, res) => {
   //console.log('req.params', req.params);
   //console.log('query params', req.query);
   getItemById(req, res);
 });
-app.post('/api/items', (req, res) => {
+app.post('/api/media', (req, res) => {
   postItem(req, res);
 });
-app.put('/api/items', (req, res) => {
+app.put('/api/media/:id', (req, res) => {
+  // TODO: implement this endpoint
   res.status(501).json({message: 'Under construction'});
 });
 
@@ -26,20 +27,5 @@ app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-// Create a server object and bind a callback function
-// to all request events
-/* const server = http.createServer((req, res) => {
-  const {url, method} = req;
-  console.log('url:', url, 'method:', method);
-  if (url === '/items' && method === 'GET') {
-    getItems(res);
-  } else if (url === '/items' && method === 'POST') {
-    postItem(req, res);
-  } else {
-    // Generic not found response
-    res.writeHead(404, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({error: '404', message: 'not found'}));
-  }
-}); */
 
 
---
+++ src/media.js
@@ -0,0 +1,81 @@
+// Dummy mock data
+const mediaItems = [
+  {
+    media_id: 9632,
+    filename: 'ffd8.jpg',
+    filesize: 887574,
+    title: 'Favorite drink',
+    description: '',
+    user_id: 1606,
+    media_type: 'image/jpeg',
+    created_at: '2023-10-16T19:00:09.000Z',
+  },
+  {
+    media_id: 9626,
+    filename: 'dbbd.jpg',
+    filesize: 60703,
+    title: 'Miika',
+    description: 'My Photo',
+    user_id: 3671,
+    media_type: 'image/jpeg',
+    created_at: '2023-10-13T12:14:26.000Z',
+  },
+  {
+    media_id: 9625,
+    filename: '2f9b.jpg',
+    filesize: 30635,
+    title: 'Aksux',
+    description: 'friends',
+    user_id: 260,
+    media_type: 'image/jpeg',
+    created_at: '2023-10-12T20:03:08.000Z',
+  },
+  {
+    media_id: 9592,
+    filename: 'f504.jpg',
+    filesize: 48975,
+    title: 'Desert',
+    description: '',
+    user_id: 3609,
+    media_type: 'image/jpeg',
+    created_at: '2023-10-12T06:59:05.000Z',
+  },
+  {
+    media_id: 9590,
+    filename: '60ac.jpg',
+    filesize: 23829,
+    title: 'Basement',
+    description: 'Light setup in basement',
+    user_id: 305,
+    media_type: 'image/jpeg',
+    created_at: '2023-10-12T06:56:41.000Z',
+  },
+];
+
+const getItems = (res) => {
+  res.json(mediaItems);
+};
+
+const postItem = (req, res) => {
+  console.log('post req body', req.body);
+  const newItem = req.body;
+  newItem.media_id = mediaItems[mediaItems.length - 1].id + 1;
+  mediaItems.push(newItem);
+  res.status(201).json({message: 'Item added', id: newItem.id});
+};
+
+const getItemById = (req, res) => {
+  const id = parseInt(req.params.id);
+  const item = mediaItems.find((item) => item.media_id === id);
+  if (item) {
+    if (req.query.format === 'plain') {
+      res.send(item.title);
+    } else {
+      res.json(item);
+    }
+  } else {
+    res.status(404).json({message: 'Item not found'});
+  }
+};
+
+export {getItems, postItem, getItemById};
