1960e3d00574b8e439f31a1b58259f9e85b7095e
mattpe <mattpe@metropolia.fi>
2024-11-25 12:58:39 +0200

    add more advanced validation error support to error handling

--- src/middlewares/error-handlers.js
+++ src/middlewares/error-handlers.js
@@ -1,42 +1,48 @@
-import {validationResult} from "express-validator";
+import {validationResult} from 'express-validator';
 
 /**
- * Create errors
- * @param {string} message Error message
- * @param {string} status HTTP status code
- * @returns {object} Error object 
+ * Custom error generator
+ * @param {string} message - error message
+ * @param {number} [status] - optional error status, default is 500
+ * @param {array} [errors] - optional array of error objects (used e.g. for validation errors)
+ * @returns {object} error object
  */
-const customError = (message, status) => {
+const customError = (message, status, errors) => {
   const error = new Error(message);
   error.status = status || 500;
+  if (errors) {
+    error.errors = errors;
+  }
   return error;
 };
 
 /**
- * Handler for all validation errors
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
- * @returns 
+ * Custom middleware for handling and formatting validation errors
+ * @param {object} req - request object
+ * @param {object} res - response object
+ * @param {function} next - next function
  */
 const validationErrorHandler = (req, res, next) => {
   // validation errors can be retrieved from the request object (added by express-validator middleware)
-  const errors = validationResult(req);
+  //const errors = validationResult(req);
+  const errors = validationResult(req, {strictParams: ['body']});
   // check if any validation errors
   if (!errors.isEmpty()) {
-    console.log('validation errors', errors.array());
-    const errorsString = errors.array().map((item) => item.path + ' ');
-    return next(customError('validation errors in: ' + errorsString, 400));
+    // console.log('validation errors', errors.array({onlyFirstError: true}));
+    // extract field names & messages from error array (only one error per field)
+    const validationErrors = errors.array({onlyFirstError: true}).map((error) => {
+      return {field: error.path, msg: error.msg};
+    });
+    return next(customError('Invalid input data', 400, validationErrors));
   }
   next();
 };
 
 /**
- * 404 error handler
- * @param {} req 
- * @param {*} res 
- * @param {*} next 
- * @returns 
+ * Generic 404 error handler
+ * @param {object} req - request object
+ * @param {object} res - response object
+ * @param {function} next - next function
  */
 const notFoundHandler = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
@@ -45,16 +51,17 @@ const notFoundHandler = (req, res, next) => {
 };
 
 /**
-* Custom default middleware for handling errors
-*/
+ * Custom default middleware for handling errors
+ */
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   res.status(err.status || 500); // default is 500 if err.status is not defined
   res.json({
     error: {
       message: err.message,
-      status: err.status || 500
-    }
+      status: err.status || 500,
+      errors: err.errors,
+    },
   });
 };
 
